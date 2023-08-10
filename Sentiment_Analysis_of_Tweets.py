2107bd8f2ea8954cd3add3fdf5ed32f5e4615aea
rizwanyoosaf37@gmail.com <rizwanyoosaf37@gmail.com>
2023-08-10 18:32:20 +0530

    created a function to clean tweets from unwanted elements

--- Sentiment_Analysis_of_Tweets.py
+++ Sentiment_Analysis_of_Tweets.py
@@ -16,3 +16,10 @@ from API_keys_Tokens import API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_
 authenticate = tweepy.OAuthHandler(API_KEY,API_KEY_SECRET)
 authenticate.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
 API = tweepy.API(authenticate)
+
+# Function to clean tweets from unwanted elements
+def tweets_cleaning(text):
+    text = re.sub(r"http\S+|www\S+|https\S+|RT", '', text, flags=re.MULTILINE)  # Remove URLs
+    text = re.sub(r'[^\w\s]', '', text)     # Remove special characters and numbers
+    text = text.lower()     # Convert to lowercase for accurate sentiment analysis
+    return text
